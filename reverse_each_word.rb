792b6a0d2b64c4232ea662cb910342184811359c
Cody Fussell <cody.fussell@gmail.com>
2019-09-11 16:14:54 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(sent)
     sent_list = sent.split()
-    end_list = sent_list.map{|a| a.reverse}
+    end_list = sent_list.collect{|a| a.reverse}
     return end_list.join(" ")
 end
